3c4ef9d15cab9597197e97f208e51a38d914c558
viniciussousav <vsv@cin.ufpe.br>
2021-08-03 16:05:18 -0300

    Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelaLoginComponent } from './tela-login/tela-login.component';
+import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
+import { TelaFitasComponent } from './tela-fitas/tela-fitas.component';
+import { TelaInicialComponent } from './tela-inicial/tela-inicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaLoginComponent,
+    TelaCadastroComponent,
+    TelaFitasComponent,
+    TelaInicialComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tela-fitas/tela-fitas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-fitas',
+  templateUrl: './tela-fitas.component.html',
+  styleUrls: ['./tela-fitas.component.css']
+})
+export class TelaFitasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
